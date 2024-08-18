446580edf7c6ca9fdf696b740259762ed94f07ec
Baris Solak <mustafabarissolak@gmail.com>
2024-08-18 17:41:26 +0300

    Yaptığınız değişikliklerin açıklaması

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+import requests
 
 API_BASE_URL = "http://127.0.0.1:5000/"
 
@@ -8,3 +9,4 @@ home_bp = Blueprint("home_bp", __name__)
 @home_bp.route("/")
 def home():
     return render_template("home.html")
+    
